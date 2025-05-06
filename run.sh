41155d2735e8e9c602743215f37d9fe06e7e13da
Hajime Terasawa <terako.studio@gmail.com>
2025-05-06 16:05:05 +0900

    feat: add minimal kernel

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xue
+
+# QEMUのファイルパス
+QEMU=qemu-system-riscv32
+# QEMUを起動
+#   -machine virt     = virtマシンとして起動する
+#   -bios default     = デフォルトのBIOS (ここではOpenSBI) を使用する
+#   -nographic        = QEMUをウィンドウなしで起動する
+#   -serial mon:stdio = QEMUの標準入出力を仮想マシンのシリアルポートに接続する
+#   --no-reboot       = 仮想マシンがクラッシュしたら、再起動せずに停止させる (デバッグに便利)
+$QEMU \
+  -machine virt \
+  -bios default \
+  -nographic \
+  -serial mon:stdio \
+  --no-reboot \
+  -kernel target/riscv32i-unknown-none-elf/debug/kernel_elf
